86eda3eec4a3701bb62505ac26e20e5f689517cd
daimakram <85197435+daimakram@users.noreply.github.com>
2022-12-07 04:21:09 +0500

    Update lanenet_postprocess.py

--- lanenet_model/lanenet_postprocess.py
+++ lanenet_model/lanenet_postprocess.py
@@ -14,6 +14,7 @@ import math
 import cv2
 import numpy as np
 import loguru
+from pandas.compat import pa_version_under1p0
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 
@@ -251,6 +252,343 @@ class _LaneNetCluster(object):
         return mask, lane_coords
 
 
+import copy
+
+def avg(ptr): # check this... why 3... wont it be a_len  
+  a = copy.deepcopy(ptr)
+  a_len = len(a)
+  try:
+    for i in range(1, a_len):
+      a[0][0] += a[i][0]
+      a[0][1] += a[i][1]
+      a[0][2] += a[i][2]
+
+    a[0][0] /= 3
+    a[0][1] /= 3
+    a[0][2] /= 3
+
+    return a[0]
+  except:
+    return a
+
+def find_vanishing(pts):
+  print("\n")
+  temp = []
+  points = []
+  arr = copy.deepcopy(pts)
+  for i in arr: # get 2 points for each lane 
+    i[0].append(1)
+    i[10].append(1)
+    temp.append( [i[0], i[10]] )
+
+  # print(temp)
+
+  for i in range(len(temp)): # get line for each lane 
+    temp[i] = np.cross(temp[i][0],temp[i][1])
+
+  # print(temp)
+
+  for i in range(len(temp)-1): # get line for each lane 
+    points.append( np.cross(temp[i], temp[i+1]) )
+  
+  vanishing_point = avg(points)
+  try: # where is there a need for try except here?
+    vanishing_point[0] = vanishing_point[0]/vanishing_point[2]
+    vanishing_point[1] = vanishing_point[1]/vanishing_point[2]
+    vanishing_point[2] = vanishing_point[2]/vanishing_point[2]
+
+    print("point is: ", vanishing_point)
+
+    return vanishing_point
+  except:
+    return vanishing_point
+
+def find_min_idx(x):
+    k = x.argmin()
+    ncol = x.shape[1]
+    return k//ncol, k%ncol
+
+def find_closest(y1, y2, a, b):
+  y1 = np.array(y1)
+  y2 = np.array(y2)
+  y1 = y1.astype(int)
+  y2 = y2.astype(int)
+  a = np.array(a)
+  b = np.array(b)
+  a = a.astype(int)
+  b = b.astype(int)
+
+  print("inside")
+  print(a[-5:,-5:])
+  print(b[-5:,-5:])
+
+  temp = np.zeros((y1.shape[0], y2.shape[0]))
+  indices = []
+
+  for i in range(y1.shape[0]): # difference of each point of y1 with y2
+    temp[i] = abs(y1[i] - y2)
+
+  # shp = temp.shape[0] // 4 # only look at starting points 
+  # temp = temp[:shp, :]
+
+  # print("new")
+  # print(find_min_idx(temp))
+  # print(np.min(temp))
+  # print( np.partition(temp, 3)[:2, 0])
+  # print( np.argpartition(temp, 3)[:2, 0])
+
+
+  # print("temp: ", temp)
+  smallest = np.zeros((temp.shape[0],)) # stores min value per y1 value
+  yi = np.zeros((temp.shape[0],)) # stores index of min value 
+
+  # assuming we will have atleast 10 points per lane 
+
+  shp = temp.shape[0]
+  # if shp > 30:
+  #   shp = shp//4
+  last_25pts = int(0.70*shp)
+  last_15pts = int(0.90*shp)
+
+  for i in range(temp.shape[0]):
+    smallest[i] = np.min(temp[i])
+    yi[i] = temp[i].argmin()
+
+  print("len",len(smallest))
+  #p1 = smallest.argmin()
+  #p2 = smallest[shp//2:shp//2 + shp//4,].argmin() + shp//2
+
+  p1 = smallest[last_25pts: last_15pts,].argmin() + last_25pts
+  p2 = smallest[last_15pts:,].argmin() + last_15pts
+  print("p1 p2")
+  print(p1)
+  print(p2)
+  # print(smallest[:10,])
+  # print(smallest[5:10,])
+  # print(shp)
+  # print(p1, smallest[p1,])
+  # print(p2, smallest[p2,])
+
+  # print(temp[:shp, :shp])
+  # print(smallest[:shp,])
+  # print(np.argpartition(smallest[:shp, ], shp-1))
+  # least_diff = np.argpartition(smallest[:shp, ], shp-1)
+
+  # print(smallest[:shp, ].argmin())
+  # smallest[smallest[:shp, ].argmin()] = 9999
+  # print(smallest[:shp, ].argmin())
+
+  # print(least_diff)
+  # print(yi[least_diff[0]])
+  v1 = a[p1]
+  v2 = b[ int(yi[p1]) ]
+  indices.append([v1, v2])
+
+  # print("v1 ",  v1)
+  # l_index = -1
+  # for i in range(1, len(least_diff)): # so y is different enough between the 2 points 
+  #   if abs(least_diff[0] - least_diff[i]) > 10: # make this more general
+  #     l_index = least_diff[i]
+  #     break
+
+  v1 = a[p2]
+  v2 = b[ int(yi[p2]) ]
+  indices.append([v1, v2])
+  # print("v1 ",  v1)
+  # min1 = smallest.argmin()
+  # ymin = yi[min1]
+  # print(min1, y1[min1], y2[ymin], a[min1], b[ymin])
+  # indices.append([a[min1], b[ymin]])
+  
+  # smallest = np.delete(smallest, min1)
+  # min2 = smallest.argmin()
+  # ymin = yi[min2]
+  # print(min2, y1[min2], y2[min2], a[min2], b[min2])
+  # indices.append([a[min2+1], b[min2+1]])
+  # print(smallest[:5,])
+  # print(smallest.argmin(), smallest[smallest.argmin()])
+  # print(y1[smallest.argmin()], y2[smallest.argmin()])
+
+  # for i in range(temp.shape[0]):
+  #   t2 = temp[i].argmin()
+  #   indices.append([a1[i], a2[t2]])
+
+  # indices = np.array(indices)
+  # smt = np.argpartition(smallest, 5)
+  # print("smallest: ", np.argpartition(smallest, 5, axis=-1))
+  # print("smallest: ", smallest[np.argpartition(smallest, 5, axis=-1)])
+
+  # sa = smt[:5,]
+  # print("smt ", smt[:5],)
+  # print("smallest ", smallest[smt[:5]],)
+  # print("sa ", sa)
+  # sm = min(sa)
+  # sx = max(sa)
+
+  # indices.append([y1[smt[0]], y2[smt[0]]])
+  # indices.append([y1[smt[1]], y2[smt[1]]])
+
+  
+  if(indices[0][0][1] > indices[1][0][1]): # sorting acc to y 
+    temp1 = indices[0]
+    indices[0] = indices[1]
+    indices[1] = temp1
+
+  if(indices[0][0][0] > indices[0][1][0]): # sorting acc to y 
+    temp1 = indices[0][0][0]
+    indices[0][0][0] = indices[0][1][0]
+    indices[0][1][0] = temp1
+  if(indices[1][0][0] > indices[1][1][0]): # sorting acc to y 
+    temp1 = indices[1][0][0]
+    indices[1][0][0] = indices[1][1][0]
+    indices[1][1][0] = temp1
+  
+  print("returning ", indices)
+
+  return indices
+
+def make_rect(ptr):
+  arr = copy.deepcopy(ptr)
+  final_pts = []
+  
+  #y1=(arr[0][0][1]+arr[0][1][1])//2
+  #y2=(arr[1][0][1]+arr[1][0][1])//2
+  #x1=(arr[0][0][0]+arr[0][1][0])//2
+  #x2=(arr[1][0][0]+arr[1][0][0])//2
+  #x1_diff=abs(arr[0][0][0]-arr[0][1][0])
+  #x2_diff=abs(arr[1][0][0]+arr[1][0][0])
+
+  #p1=[x1-x1_diff,y1]
+  #p3=[x2-x2_diff,y2]
+  #p2=[x1+x1_diff,y1]
+  #p4=[x2+x2_diff,y2]
+  
+  print(arr)
+
+  # x1_avg = (arr[0][0][0] + arr[0][1][0])//2  # p1x + p2x 
+  # x2_avg = (arr[1][0][0] + arr[1][1][0])//2  # p1x + p2x 
+  y_avg = (arr[0][0][1] + arr[0][1][1])//2 # p1y + p2y 
+  dy = abs(arr[1][0][1] - arr[0][0][1]) # p3y - p1y 
+  dx1 = abs(arr[0][0][0] - arr[0][1][0]) 
+  dx2 = abs(arr[1][0][0] - arr[1][1][0]) 
+
+  if dx1 < dx2:
+    x_avg = (arr[0][0][0] + arr[0][1][0]) // 2 
+    dx = dx1
+  else: 
+    x_avg = (arr[1][0][0] - arr[1][1][0]) // 2 
+    dx = dx2
+
+
+  print(x_avg, dx)
+
+  # if(x1_avg < x2_avg):
+  #   dx = abs(arr[0][0][0] - arr[0][1][0]) # p1x - p2x 
+  # else: 
+  #   dx = abs(arr[1][0][0] - arr[1][1][0]) # p1x - p2x 
+
+  p1 = [x_avg - dx//4, y_avg ]
+  p2 = [x_avg + dx//4, y_avg ]
+  p3 = [x_avg - dx//4, y_avg + dy]
+  p4 = [x_avg + dx//4, y_avg + dy]
+
+  # for i in arr:
+  #   yf = (i[0][1] + i[1][1])/2 # my y 
+  #   x = [i[0][0], i[1][0]]
+  #   x.sort()
+  #   dx = (x[1] - x[0])/4
+  #   xm = x[0] + dx
+  #   xx = x[1] - dx
+  #   final_pts.append([xm, yf])
+  #   final_pts.append([xx, yf])
+    
+  # # arr.sort()
+
+  # p1 = arr[0][0]
+  # p2 = arr[0][1]
+  # p3 = arr[1][0]
+  # p4 = arr[1][1]
+
+  # dx = 2
+
+  # y1_avg = (p3[1]+p1[1]) / 2
+  # x1_avg = (p3[0]+p1[0]) / 2
+  # y2_avg = (p4[1]+p2[1]) / 2
+  # x2_avg = (p4[0]+p2[0]) / 2
+  # xf = (x1_avg + x2_avg) / 2
+  # yf = (y1_avg + y2_avg) / 2
+  # x1 = [xf-dx, yf-dx]
+  # x2 = [xf+dx, yf-dx]
+  # x3 = [xf-dx, yf+dx]
+  # x4 = [xf+dx, yf+dx]
+
+  return [p1,p2,p3,p4]
+  # return final_pts
+
+def dynamicHmg(pts):
+  arr = copy.deepcopy(pts)
+  arr_len = len(arr)
+  if arr_len <= 1: 
+    print("\n not enough lanes ", arr_len)
+    return
+  
+  midpoint = arr_len//2
+
+  
+
+  print("total lanes ", arr_len)
+
+  midpoint = 0
+  y1 = [j[1] for j in arr[midpoint]] # fills array with y values 
+  y2 = [j[1] for j in arr[midpoint+1]]
+
+
+  # print(arr[midpoint-1])
+  # print(arr[midpoint])
+
+  # print(y1)
+  # print(y2)
+  total_points = find_closest(y1,y2, arr[midpoint],  arr[midpoint+1])
+  rec_points = make_rect(total_points)
+  # indices = []
+
+  # for i in range(len(y1)):
+  #   for j in range(len(y2)):
+  #     if y1[i] == y2[j]: # if they are equal then store their index
+  #       indices.append([i,j])
+
+  # total_points = []
+  # for i in (indices):
+  #   t1 = arr[midpoint-1][i[0]]
+  #   t2 = arr[midpoint][i[1]]
+  #   total_points.append([t1, t2])
+
+  # final_pts = []
+
+  # for i in range(len(total_points)): # fix this | it should be acc to which is smaller 
+  #   if total_points[i][1] < total_points[i][0]:
+  #     final_pts.append(total_points[i][1])
+  #     final_pts.append(total_points[i][0])
+  #   else:
+  #     final_pts.append(total_points[i][0])
+  #     final_pts.append(total_points[i][1])
+
+
+  final_pts = []
+  final_pts.append(total_points[0][0])
+  final_pts.append(total_points[0][1])
+  final_pts.append(total_points[1][0])
+  final_pts.append(total_points[1][1])
+
+  final_pts = np.array(final_pts).astype(int)
+  rec_points = np.array(rec_points).astype(int)
+
+  print("total points: ", final_pts )
+  print("rect points: ", rec_points )
+  h, _ = cv2.findHomography(final_pts, rec_points, cv2.RANSAC, 5.0)
+  print("homography -- ", h)
+  return final_pts, rec_points, h
+
 class LaneNetPostProcessor(object):
     """
     lanenet post process for lane generation
@@ -391,6 +729,8 @@ class LaneNetPostProcessor(object):
 
             src_lane_pts.append(lane_pts)
 
+        vanishing_point = find_vanishing(src_lane_pts)
+        fp, rp, h_mat = dynamicHmg(src_lane_pts)
         # tusimple test data sample point along y axis every 10 pixels
         source_image_width = source_image.shape[1]
         for index, single_lane_pts in enumerate(src_lane_pts):
@@ -438,6 +778,10 @@ class LaneNetPostProcessor(object):
             'mask_image': mask_image,
             'fit_params': fit_params,
             'source_image': source_image,
+            'vanishing_point':vanishing_point,
+            'homography':h_mat,
+            'fp':fp,
+            'rp':rp
         }
 
         return ret
